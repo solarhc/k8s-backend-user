729f20d22c551f784c5199a1469e5433290ecec5
solarhc <solarhc@naver.com>
2025-06-11 17:20:30 +0900

    FeignClient

--- src/main/java/com/welab/k8s_backend_user/api/open/UserController.java
+++ src/main/java/com/welab/k8s_backend_user/api/open/UserController.java
@@ -1,5 +1,6 @@
 package com.welab.k8s_backend_user.api.open;
 
+import com.welab.k8s_backend_user.remote.alim.RemoteAlimService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/api/user/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class UserController {
+    private final RemoteAlimService remoteAlimService;
+
     @GetMapping(value = "/test")
     public String test() {
-        return "Hello Kubernetes";
+        String remoteMessage = remoteAlimService.hello().getData();
+
+        return "웰컴 투 유저 서비스: 리모트 메시지 = " + remoteMessage;
     }
 }

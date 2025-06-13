88960f1a68e4edaa2a444f54cfca9b55751b0366
solarhc <solarhc@naver.com>
2025-06-13 10:19:14 +0900

    config-map

--- src/main/java/com/welab/k8s_backend_user/advice/ApiCommonAdvice.java
+++ src/main/java/com/welab/k8s_backend_user/advice/ApiCommonAdvice.java
@@ -22,6 +22,8 @@ public class ApiCommonAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({BadParameter.class})
     public ApiResponseDto<String> handleBadParameter(BadParameter e) {
+        e.printStackTrace();
+
         return ApiResponseDto.createError(
                 e.getErrorCode(),
                 e.getErrorMessage()
@@ -31,6 +33,8 @@ public class ApiCommonAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({NotFound.class})
     public ApiResponseDto<String> handleNotFound(NotFound e) {
+        e.printStackTrace();
+
         return ApiResponseDto.createError(
                 e.getErrorCode(),
                 e.getErrorMessage()
@@ -40,6 +44,8 @@ public class ApiCommonAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({ClientError.class})
     public ApiResponseDto<String> handleClientError(ClientError e) {
+        e.printStackTrace();
+
         return ApiResponseDto.createError(
                 e.getErrorCode(),
                 e.getErrorMessage()
@@ -49,6 +55,8 @@ public class ApiCommonAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({NoResourceFoundException.class})
     public ApiResponseDto<String> handleNoResourceFoundException(NoResourceFoundException e) {
+        e.printStackTrace();
+
         return ApiResponseDto.createError(
                 "NoResource",
                 "리소스를 찾을 수 없습니다.");
@@ -57,6 +65,8 @@ public class ApiCommonAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({MethodArgumentNotValidException.class})
     public ApiResponseDto<ParameterErrorDto.FieldList> handleArgumentNotValidException(MethodArgumentNotValidException e) {
+        e.printStackTrace();
+
         BindingResult result = e.getBindingResult();
         ParameterErrorDto.FieldList fieldList = ParameterErrorDto.FieldList.of(result);
 
@@ -67,6 +77,8 @@ public class ApiCommonAdvice {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler({Exception.class})
     public ApiResponseDto<String > handleException(Exception e) {
+        e.printStackTrace();
+
         return ApiResponseDto.createError(
                 "ServerError",
                 "서버 에러입니다.");
